07f15e4acb013387a2da09e1e80da45e6a857dda
kelvin <muhammadfalahalfarisi@gmail.com>
2023-08-09 10:13:25 +0800

    mempelajari number di js

---
+++ standartLibrary/Number.js
@@ -0,0 +1,18 @@
+const input = '12345';
+const number = Number(input);
+
+console.info(typeof input)
+console.info(typeof number)
+console.info(number)
+
+console.info(Number('salah'));
+
+console.info(Number.MIN_VALUE);
+console.info(Number.MAX_VALUE);
+console.info(Number.MIN_SAFE_INTEGER);
+console.info(Number.MAX_SAFE_INTEGER);
+
+
+
+
+
